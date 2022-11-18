c78f6790d56112ab921995f0d7be07bfc6c98aa7
2106050212 <113961269+2106050212@users.noreply.github.com>
2022-11-18 14:07:37 +0800

    Update tower.py

--- tower.py
+++ tower.py
@@ -56,3 +56,36 @@ class ArchTower(Tower):
             self.archer_count = 0
         archer = self.archer_imgs[self.archer_count // 10]
         win.blit(archer, ((self.x - 25), (self.y - archer.get_height() - 25)))
+        
+     def attack(self, enemies):
+        count = [0, 0]
+        self.inRange = False
+        enemy_inRange = []
+        for enemy in enemies:
+            x = enemy.x
+            y = enemy.y
+            dis = math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)  # distance
+            if dis < self.range:
+                self.inRange = True
+                enemy_inRange.append(enemy)
+
+        # 选定最近的一个敌人攻击
+        enemy_inRange.sort(key=lambda x: -x.distance)
+        if len(enemy_inRange) > 0:
+            first_enemy = enemy_inRange[0]
+            if time.time() - self.timer >= 0.5:
+                self.timer = time.time()
+                if first_enemy.die(self.damage):
+                    count[0] = first_enemy.count_coin
+                    count[1] = first_enemy.count_score
+                    enemies.remove(first_enemy)
+            # if first_enemy.x > self.x and not self.left:
+            #     self.left = True
+            #     for x, img in enumerate(self.archer_imgs):
+            #         self.archer_imgs[x] = pygame.transform.flip(img, True, False)
+            # elif self.left and first_enemy.x < self.x:
+            #     self.left = False
+            #     for x, img in enumerate(self.archer_imgs):
+            #         self.archer_imgs[x] = pygame.transform.flip(img, True, False)
+
+        return count
