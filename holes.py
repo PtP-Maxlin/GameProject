ee0e70921fcba4690abb4f4c8e8dacdab929c53e
2106050212 <113961269+2106050212@users.noreply.github.com>
2022-11-19 18:55:53 +0800

    Update holes.py

--- holes.py
+++ holes.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Hole:
-    def __init__(self):
+    def __init__(self, x, y):
         self.hole = pygame.image.load('塔防游戏素材/按钮/坑1.jpg')
         self.hole_menu = pygame.image.load('塔防游戏素材/按钮/建造2.png')
         self.hole_menu_tower1 = pygame.image.load('塔防游戏素材/按钮/选项.png')
@@ -20,8 +20,8 @@ class Hole:
         self.hole_menu_tower2_rect = self.hole_menu_tower2.get_rect()
         self.hole_menu_tower3_rect = self.hole_menu_tower3.get_rect()
         # x, y 改一下
-        self.hole_rect.x = 10
-        self.hole_rect.y = 20
+        self.hole_rect.x = x
+        self.hole_rect.y = y
         self.hole_menu_rect.x = self.hole_rect.x - 5
         self.hole_menu_rect.y = self.hole_rect.y - 125
         self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
@@ -33,6 +33,9 @@ class Hole:
 
         self.menu = True
         self.lock = True
+        self.bool = True
+        self.selected = [False, False, False]
+
 
     def draw(self, win):
         win.blit(self.hole, self.hole_rect)
@@ -80,113 +83,3 @@ class Hole:
     def flush(self):
         self.menu = True
         self.lock = True
-
-
-
-'''第一关'''
-
-
-class Hole_1_1(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 25
-        self.hole_rect.y = 435
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-class Hole_1_2(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 285
-        self.hole_rect.y = 411
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-class Hole_1_3(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 512
-        self.hole_rect.y = 175
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-class Hole_1_4(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 741
-        self.hole_rect.y = 381
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-class Hole_1_5(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 954
-        self.hole_rect.y = 184
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-class Hole_1_6(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 1008
-        self.hole_rect.y = 418
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
-
-
-
-class Hole_1_7(Hole):
-    def __init__(self):
-        super().__init__()
-        self.hole_rect.x = 965
-        self.hole_rect.y = 640
-        self.hole_menu_rect.x = self.hole_rect.x - 5
-        self.hole_menu_rect.y = self.hole_rect.y - 125
-        self.hole_menu_tower1_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower1_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower2_rect.x = self.hole_rect.x + 96
-        self.hole_menu_tower2_rect.y = self.hole_rect.y - 110
-        self.hole_menu_tower3_rect.x = self.hole_rect.x - 1
-        self.hole_menu_tower3_rect.y = self.hole_rect.y - 37
